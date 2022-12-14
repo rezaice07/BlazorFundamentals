72b3e99874e1d7ddfabab51ca8bd773cdf7b06a3
Md. Rezoyanul Islam <rezaice07@gmail.com>
2022-12-14 08:42:28 +0600

    added new items

---
+++ BlazorApp/Data/ContactModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Data
+{
+    [Serializable]
+    public class ContactModel
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
