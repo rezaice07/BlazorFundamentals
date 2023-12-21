b3a97fef4efa16bad620e6e1e9b0a80ed8ca7fb3
Md. Rezoyanul Islam <rezaice07@gmail.com>
2023-12-21 08:41:11 +0600

    remove .vs

--- BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e65aed02d39be62d8106d2c0eb70d62750ac1a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca863cfef3c4a20feb574a7e976ef25d5d55b6ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
